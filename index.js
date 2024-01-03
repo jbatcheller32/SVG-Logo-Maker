fa38298d48a65527271efbb1151be210a8ddce7e
J Mac <jbatcheller123@gmail.com>
2024-01-03 16:00:18 -0700

    I got the jest installed and I have a failing test running. I am not totally sure how to get the shapes to work. I am having some trouble with the logic. Doing some more research after this commit

--- index.js
+++ index.js
@@ -1,65 +1,74 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Circle = require('./lib/circle');
-const Square = require('./lib/square');
-const Triangle = require('./lib/triangle'); 
+const Shape = require('./lib/shapes');
+const Triangle = require('./lib/shapes');
+const Circle = require('./lib/shapes');
+const Square = require('./lib/shapes');
 
 
 // prompts for the user to create their desired logo
 
 
-const options =  [(
+const questions = [
 
-        {
-            type: 'input',
-            message: 'Enter text for logo (no more than 3 characters)',
-            name: 'logo text'
-        },
+    {
+        type: 'input',
+        message: 'Enter text for logo (no more than 3 characters)',
+        name: 'logoText'
+    },
 
-        {
-            type: 'input',
-            message: 'Enter text color',
-            name: 'text color'
-        },
+    {
+        type: 'input',
+        message: 'Enter text color',
+        name: 'textColor'
+    },
 
-        {
-            type: 'list',
-            message: 'Choose logo shape',
-            choices: ['Square', 'Triangle', 'Circle']
-        },
+    {
+        type: 'list',
+        message: 'Choose logo shape',
+        choices: ['Square', 'Triangle', 'Circle'],
+        name: 'shapeList'
+    },
 
-        {
-            type: 'input',
-            message: 'Enter shape color',
-            name: 'shape color'
+    {
+        type: 'input',
+        message: 'Enter shape color',
+        name: 'shapeColor'
 
+    }
+
+
+];
+
+// for the prompts we are using all the "names" for the if statments to create the svg based on the user input
+
+inquirer.prompt(questions)
+    .then((responses) => {
+
+        const { logoText, textColor, shapeList, shapeColor } = responses;
+        const shapeObj = new Shape();
+        let svg = ' ';
+
+        shapeObj.setColor(shapeColor);
+
+        if (shapeList === Triangle) {
+            const triangle = new Triangle()
+            triangle.setColor(shapeColor)
+            svg = Triangle.render()
+        // } if (shapeList === Circle) {
+        //     Circle.setColor(shapeColor)
+        // } if (shapeList === Square) {
+        //     Square.setColor(shapeColor) 
+            
+         } else {
+            return;
         }
 
-)
-    ]
 
-    //function to write the file for SVG logo
-
-    function writeToFile(fileName, data) {
-
-        fs.writeFile(fileName, data, (err) => {
-            err ? console.error(err) : console.log('Creating your SVG logo...')
-        })
-    }; 
-
-
-// function to initialize the questions and responses
-
-    function init() {
 
         
-    
-        inquirer.prompt(options)
-        .then((response) => {
-            writeToFile('logo.svg', Circle(response));
-        })
-    
-    };
-    
-    init();
+    });
+
+
+
 
